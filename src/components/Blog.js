1e1a84b5e66c2260ddb87a1fc4fbad490e971530
Vivianna Tilander <viva95@windowslive.com>
2020-02-24 14:21:32 +0200

    Tehtävä 5.7*: blogilistan frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const style = {
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 2,
+    borderColor: 'lightgrey',
+    borderRadius: 5,
+    marginBottom: 4
+  }
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const like = () => {
+
+  }
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author} <button onClick={toggleShowDetails}>{showDetails ? 'Hide' : 'View'}</button>
+      { showDetails ?
+          <>
+            <br/>{blog.url}
+            <br/> Likes: {blog.likes} <button onClick={like}>Like</button>
+            <br/>{blog.user.name}
+          </> :
+          ''
+      }
+    </div>
+  )
+}
 
 export default Blog
